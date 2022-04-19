fcd6f1983b7b81a912a91966186c5bd243433e1e
LuizFelipe883 <luizfelipedesouza94@gmail.com>
2022-04-19 15:59:30 -0300

    teste para sortear um numero aleatorio

---
+++ NumerosAleatorios.js
@@ -0,0 +1,6 @@
+const _ = require("lodash")
+
+const numInicial = 1
+const numFinal = 500
+
+console.log(_.random(numInicial, numFinal))
---
+++ README.md
@@ -0,0 +1,1 @@
+Coloque o numero inicial e o numero final para funcionar corretamente

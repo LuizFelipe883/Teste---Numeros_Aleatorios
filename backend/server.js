439c9b267e3afee3e42f84fa28dfb0a9f34216f3
LuizFelipe883 <luizfelipedesouza94@gmail.com>
2022-05-17 23:08:30 -0300

    desenvolvendo

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 const path = require('path');
-const DataBase = require("./index")
+const DataBase = require("../frontend/assets/js/index")
 const bodyParser = require("body-parser")
 const PORT = 3003;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    app.use(cors());
+    next();
+})
+
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, './../../../frontend'));
+app.set('views', path.join(__dirname, '../frontend/pages'));
 
 app.get("/sorteador", async (req, res) => {
     const sorteio = await DataBase.sorteador({
@@ -20,7 +27,7 @@ app.get("/sorteador", async (req, res) => {
 
 //rota da pagina incial
 app.get("/", function (req, res) {
-    res.render("./pages/main", { pageInicial: './layouts/index.ejs' })
+    res.render("./main", { pageInicial: './layouts/index.ejs' })
 })
 
 app.listen(PORT, () => { console.log(`SERVIDOR ONLINE --> http://localhost:${PORT}/`)
